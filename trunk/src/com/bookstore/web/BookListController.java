75daf02e7ca1de7f73f83668b0912cdcdd60962c
DeliaMei <DeliaMei@06929fae-c0f1-11dd-90dc-1bd47c2f18c4>
2008-12-04 17:32:55 +0000

    1)Changed var name, latName to lastName (yes, anal compulsive) 2)Combined latest & all into one jsp

--- trunk/src/com/bookstore/web/BookListController.java
+++ trunk/src/com/bookstore/web/BookListController.java
@@ -16,7 +16,23 @@ public class BookListController extends ParameterizableViewController {
 	public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("bookList", bookManager.getBooks());
+		
+		// All or Latest
+		Object attributeValue1 = null;
+		Object attributeValue2 = null;
+		String filter = request.getParameter("filter");
+		
+		if(filter!=null && filter.equals("latest")){
+			attributeValue1 = bookManager.getLatest();
+			attributeValue2 = "Latest";
+		}
+		else {
+			attributeValue1 = bookManager.getBooks();
+			attributeValue2 = "All";
+		}
+		
+		mav.addObject("bookList", attributeValue1);
+		mav.addObject("title", attributeValue2);
 		mav.setViewName(getViewName());
 		return mav;
 	}
